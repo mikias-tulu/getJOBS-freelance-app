63ab0e10c5a3f7c192f54381c1258acfd2a07ced
Miki T <miki.tulu@gmail.com>
2022-12-17 21:17:32 +0300

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,7 +24,10 @@ class MyApp extends StatelessWidget {
                 body: Center(
                     child: Text(
                   'Freelacne app initialized',
-                  style: TextStyle(fontFamily: 'signatra'),
+                  style: TextStyle(
+                      fontFamily: 'signatra',
+                      color: Colors.green,
+                      fontSize: 30),
                 )),
               ),
             );
