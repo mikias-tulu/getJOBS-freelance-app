acf4a21ae43c6ab6ffcf91b8cd1d9ea1be980bc3
Miki T <miki.tulu@gmail.com>
2022-12-17 23:28:38 +0300

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
-
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
