01b931d18ebcea84ef86164912ef39427f70ff1f
Miki T <miki.tulu@gmail.com>
2022-12-24 23:12:42 +0300

    initial commit

---
+++ lib/utils/variables.dart
@@ -0,0 +1,4 @@
+String loginUrlImage = 'https://vistapointe.net/images/building-4.jpg';
+String singUpUrlImage =
+    'https://img.freepik.com/free-photo/skyscrapers-from-low-angle-view_1359-568.jpg?w=2000';
+String forgetPasswordUrlImage = 'https://wallpaperaccess.com/full/1393537.jpg';
