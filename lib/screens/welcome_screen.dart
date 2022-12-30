0bf1c4d21b528d1817d58d3e123ed84482bc39ac
Miki T <miki.tulu@gmail.com>
2022-12-30 16:47:32 +0300

    Create welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:freelance_app/screens/home_screen.dart';
+
+import '../widgets/custom_button.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background.png"))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const SizedBox(
+              height: 130,
+              //width: 180,
+              child: Image(
+                  image: AssetImage("assets/images/logo.png"),
+                  fit: BoxFit.fill),
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+              buttonText: "Login",
+              buttonColor: Colors.black,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) => const HomeScreen() //const LoginScreen(),
+                      ),
+                );
+              },
+            ),
+            CustomButton(
+              buttonText: "Register",
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (_) => const HomeScreen() //SignUpScreen(),
+                      ),
+                );
+              },
+            ),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "getJOBS get the job done!",
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
