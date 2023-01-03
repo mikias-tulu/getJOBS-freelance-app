8686335fac94b41ac209825cf607edb825e2d880
Miki T <miki.tulu@gmail.com>
2023-01-03 20:31:40 +0300

    Create colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const red = Colors.red;
+const white = Colors.white;
+const black = Colors.black;
+const grey = Colors.grey;
+const brown = Colors.brown;
+const yellow = Color.fromRGBO(245, 186, 65, 1);
