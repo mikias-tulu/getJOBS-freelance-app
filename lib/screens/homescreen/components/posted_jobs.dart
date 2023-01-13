94fadbb9ed58ae24cfdcdb537f680df362836cf0
Miki T <miki.tulu@gmail.com>
2023-01-13 23:10:59 +0300

    update posted_jobs.dart

--- lib/screens/homescreen/components/posted_jobs.dart
+++ lib/screens/homescreen/components/posted_jobs.dart
@@ -17,67 +17,70 @@ class _PostedjobState extends State<Postedjob> {
         scrollDirection: Axis.vertical,
         itemCount: 5,
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 250,
-            width: 200,
-            margin: const EdgeInsets.all(8),
-            decoration: const BoxDecoration(
-                color: white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30)),
-                boxShadow: [
-                  BoxShadow(
-                    color: grey,
-                    offset: Offset(15, 5),
-                    blurRadius: 30,
-                  )
-                ]),
-            child: Column(
-              children: [
-                const ListTile(
-                  leading: Text("Company",
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: yellow,
-                          fontWeight: FontWeight.w800)),
-                  trailing: Text("contractual",
-                      style: TextStyle(
-                        fontSize: 10,
-                      )),
-                ),
-                const ListTile(
-                    trailing: Text("payment type",
+          return InkWell(
+            onTap: () {},
+            child: Container(
+              height: 250,
+              width: 200,
+              margin: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                  color: white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: grey,
+                      offset: Offset(15, 5),
+                      blurRadius: 30,
+                    )
+                  ]),
+              child: Column(
+                children: [
+                  const ListTile(
+                    leading: Text("Company",
                         style: TextStyle(
-                          fontSize: 13,
-                        ))),
-                const ListTile(
-                  leading: Text("job title",
-                      style: TextStyle(
+                            fontSize: 18,
+                            color: yellow,
+                            fontWeight: FontWeight.w800)),
+                    trailing: Text("contractual",
+                        style: TextStyle(
                           fontSize: 10,
-                          color: yellow,
-                          fontWeight: FontWeight.w900)),
-                ),
-                ListTile(
-                  leading: const Text("posted time",
-                      style: TextStyle(
-                        fontSize: 10,
-                      )),
-                  trailing: ElevatedButton(
-                    onPressed: () => {},
-                    child: const Text(
-                      "See Details ->",
-                      style: TextStyle(
-                          fontFamily: "Nexa-Thin",
+                        )),
+                  ),
+                  const ListTile(
+                      trailing: Text("payment type",
+                          style: TextStyle(
+                            fontSize: 13,
+                          ))),
+                  const ListTile(
+                    leading: Text("job title",
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: yellow,
+                            fontWeight: FontWeight.w900)),
+                  ),
+                  ListTile(
+                    leading: const Text("posted time",
+                        style: TextStyle(
                           fontSize: 10,
-                          fontWeight: FontWeight.normal,
-                          color: red),
+                        )),
+                    trailing: ElevatedButton(
+                      onPressed: () => {},
+                      child: const Text(
+                        "See Details ->",
+                        style: TextStyle(
+                            fontFamily: "Nexa-Thin",
+                            fontSize: 10,
+                            fontWeight: FontWeight.normal,
+                            color: red),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
