be2724317829dc15ff7b097b559abce430f03421
Miki T <miki.tulu@gmail.com>
2023-01-15 17:53:13 +0300

    signup page initial commit

---
+++ lib/screens/user/signup_screen.dart
@@ -0,0 +1,208 @@
+import 'package:freelance_app/services/firebase_auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:freelance_app/widgets/custom_textfield.dart';
+import 'package:freelance_app/widgets/custom_button.dart';
+import 'login_screen.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                      icon: const Icon(Icons.arrow_back_ios_sharp),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text("Hello!  Register to get \nStarted",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              CustomTextfield(
+                myController: _usernameController,
+                hintText: "Username",
+                isPassword: false,
+              ),
+              CustomTextfield(
+                myController: _emailController,
+                hintText: "Email",
+                isPassword: false,
+              ),
+              CustomTextfield(
+                myController: _passwordController,
+                hintText: "Password",
+                isPassword: true,
+              ),
+              CustomTextfield(
+                myController: _confirmPasswordController,
+                hintText: "Confirm Password",
+                isPassword: true,
+              ),
+              CustomButton(
+                buttonText: "Register",
+                buttonColor: Colors.black,
+                textColor: Colors.white,
+                onPressed: () async {
+                  try {
+                    await FirebaseAuthService().signup(
+                        _emailController.text.trim(),
+                        _passwordController.text.trim());
+
+                    if (!mounted) return;
+
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginScreen()));
+                  } on FirebaseException catch (e) {
+                    debugPrint(e.message);
+                  }
+
+                  // Navigator.push(context,
+                  //     MaterialPageRoute(builder: (_) => const LoginScreen()));
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 1,
+                      width: MediaQuery.of(context).size.height * 0.15,
+                      color: Colors.grey,
+                    ),
+                    const Text("Or Register with"),
+                    Container(
+                      height: 1,
+                      width: MediaQuery.of(context).size.height * 0.16,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            FontAwesomeIcons.facebookF,
+                            color: Colors.blue,
+                          ),
+                          onPressed: () {},
+                        )),
+                    Container(
+                      height: 50,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(
+                          FontAwesomeIcons.google,
+                          // color: Colors.blue,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                        height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            FontAwesomeIcons.apple,
+                            // color: Colors.blue,
+                          ),
+                          onPressed: () {},
+                        ))
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(48, 8, 8, 8.0),
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text("Already have an account?",
+                        style: TextStyle(
+                          color: Color(0xff1E232C),
+                          fontSize: 15,
+                        )),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => const LoginScreen()));
+                      },
+                      child: const Text("  Login Now",
+                          style: TextStyle(
+                            color: Color(0xff35C2C1),
+                            fontSize: 15,
+                          )),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
