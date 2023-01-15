f6a4212f026cd4f96c688295cf9ffa7b2ad84100
Miki T <miki.tulu@gmail.com>
2023-01-15 17:52:22 +0300

    added logout functionality

--- lib/screens/homescreen/sidebar.dart
+++ lib/screens/homescreen/sidebar.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:freelance_app/utils/colors.dart';
 
+import '../../config/themes/user_state.dart';
+
 class SideBar extends StatelessWidget {
-  const SideBar({super.key});
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  SideBar({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,51 +14,64 @@ class SideBar extends StatelessWidget {
         child: Padding(
       padding: const EdgeInsets.only(top: 50.0),
       child: SizedBox(
-          width: 280,
-          child: Drawer(
-              child: ListView(
-                  padding: const EdgeInsets.only(top: 50, left: 10),
-                  children: const [
-                ListTile(
-                  leading: Icon(
-                    Icons.post_add,
-                    size: 35,
-                    color: yellow,
-                  ),
-                  title: Text("job's posted",
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+        width: 280,
+        child: Drawer(
+          child: ListView(
+            padding: const EdgeInsets.only(top: 50, left: 10),
+            children: [
+              const ListTile(
+                leading: Icon(
+                  Icons.post_add,
+                  size: 35,
+                  color: yellow,
                 ),
-                ListTile(
-                  leading: Icon(
-                    Icons.history,
-                    size: 35,
-                    color: yellow,
-                  ),
-                  title: Text("job history",
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                title: Text("job's posted",
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              ),
+              const ListTile(
+                leading: Icon(
+                  Icons.history,
+                  size: 35,
+                  color: yellow,
                 ),
-                ListTile(
-                  leading: Icon(
-                    Icons.settings,
-                    size: 35,
-                    color: yellow,
-                  ),
-                  title: Text("Settings",
-                      style:
-                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                title: Text("job history",
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              ),
+              const ListTile(
+                leading: Icon(
+                  Icons.settings,
+                  size: 35,
+                  color: yellow,
                 ),
-                ListTile(
-                    leading: Icon(
-                      Icons.logout,
-                      size: 35,
-                      color: yellow,
-                    ),
-                    title: Text("Log out",
-                        style: TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.bold))),
-              ]))),
+                title: Text("Settings",
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              ),
+              ListTile(
+                onTap: () {
+                  _auth.signOut();
+                  Navigator.canPop(context) ? Navigator.pop(context) : null;
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const UserState()));
+                },
+                leading: const Icon(
+                  Icons.logout,
+                  size: 35,
+                  color: yellow,
+                ),
+                title: const Text(
+                  "Log out",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     ));
   }
 }
