45b05a8231ed6c23227f353e7555444558dfc09f
Miki T <miki.tulu@gmail.com>
2023-01-28 23:08:01 +0300

    folder restructured

---
+++ lib/config/firebase_auth_service.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseAuthService {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future login(String email, String password) async {
+    await auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future signup(String email, String password) async {
+    await auth.createUserWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future logininwithgoogle() async {
+    GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    AuthCredential myCredential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    UserCredential user =
+        await FirebaseAuth.instance.signInWithCredential(myCredential);
+
+    debugPrint(user.user?.displayName);
+  }
+}
