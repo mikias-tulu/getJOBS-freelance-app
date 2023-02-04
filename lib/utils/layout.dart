6d49e15cb9c91b1b54be297fecdb780f40576f49
Miki T <miki.tulu@gmail.com>
2023-02-04 22:29:22 +0300

    created layout.dart

---
+++ lib/utils/layout.dart
@@ -0,0 +1,11 @@
+class layout {
+  static const padding = 16.0;
+  static const radius = 16.0;
+  static const elevation = 5.0;
+
+  static const curvedNavBarHeight = 50.0;
+
+  static const iconSmall = 20.0;
+  static const iconMedium = 30.0;
+  static const iconLarge = 35.0;
+}
