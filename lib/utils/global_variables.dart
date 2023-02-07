98f25e3402b8c07b195643b74d3013078ff2f135
Miki T <miki.tulu@gmail.com>
2023-02-07 23:34:23 +0300

    initial commit to global_variables.dart

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+String loginUrlImage =
+    'https://images.unsplash.com/photo-1527511624584-388b58b9620c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80';
+
+String signUpUrlImage =
+    'https://images.unsplash.com/photo-1565347878219-552c839f1447?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGJ1c2luZXNzfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60';
+
+String forgetPasswordUrlImage =
+    'https://miro.medium.com/max/720/1*LerIi8K3mAj3ZdnCjad61Q.jpeg';
+
+List<String> jobCategories = [
+  'Architecture and Construction',
+  'Education and Training',
+  'Development - Programming',
+  'Business',
+  'Information Technology',
+  'Human Resources',
+  'Marketing',
+  'Design',
+  'Accounting',
+];
+
+String? name;
+String? user_image;
+String? address;
+String? id;
+
+String? avatarURL;
+NetworkImage? avatarImage;
