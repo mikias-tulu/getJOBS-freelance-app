94489709e3b8f2d570ae81bc4b5d468e77c068d4
Miki T <miki.tulu@gmail.com>
2023-02-07 23:32:26 +0300

    firebase retrival  for posted_jobs

--- lib/screens/homescreen/components/posted_jobs.dart
+++ lib/screens/homescreen/components/posted_jobs.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
+import 'package:freelance_app/utils/global_variables.dart';
 
-import 'package:freelance_app/utils/colors.dart';
+import 'package:freelance_app/utils/layout.dart';
+import 'package:freelance_app/utils/txt.dart';
+import 'package:freelance_app/utils/clr.dart';
+import 'package:freelance_app/widgets/job_tile.dart';
 
 class Postedjob extends StatefulWidget {
   const Postedjob({super.key});
@@ -10,81 +16,207 @@ class Postedjob extends StatefulWidget {
 }
 
 class _PostedjobState extends State<Postedjob> {
+  String? jobCategoryFilter;
+
+  void getMyData() async {
+    final DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    setState(() {
+      name = userDoc.get('name');
+      user_image = userDoc.get('user_image');
+      address = userDoc.get('address');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMyData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: 5,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () {},
-            child: Container(
-              height: 250,
-              width: 200,
-              margin: const EdgeInsets.all(8),
-              decoration: const BoxDecoration(
-                  color: white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: grey,
-                      offset: Offset(15, 5),
-                      blurRadius: 30,
-                    )
-                  ]),
-              child: Column(
-                children: [
-                  const ListTile(
-                    leading: Text("Company",
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: yellow,
-                            fontWeight: FontWeight.w800)),
-                    trailing: Text("contractual",
-                        style: TextStyle(
-                          fontSize: 10,
-                        )),
-                  ),
-                  const ListTile(
-                      trailing: Text("payment type",
-                          style: TextStyle(
-                            fontSize: 13,
-                          ))),
-                  const ListTile(
-                    leading: Text("job title",
-                        style: TextStyle(
-                            fontSize: 10,
-                            color: yellow,
-                            fontWeight: FontWeight.w900)),
-                  ),
-                  ListTile(
-                    leading: const Text("posted time",
-                        style: TextStyle(
-                          fontSize: 10,
-                        )),
-                    trailing: ElevatedButton(
-                      onPressed: () => {},
-                      child: const Text(
-                        "See Details ->",
-                        style: TextStyle(
-                            fontFamily: "Nexa-Thin",
-                            fontSize: 10,
-                            fontWeight: FontWeight.normal,
-                            color: red),
-                      ),
-                    ),
-                  ),
-                ],
+      child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance
+            .collection('jobPosted')
+            .where('category', isEqualTo: jobCategoryFilter)
+            .where('recruiting', isEqualTo: true)
+            .orderBy('created', descending: false)
+            .snapshots(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.active) {
+            if (snapshot.data?.docs.isNotEmpty == true) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                  top: 0,
+                  bottom: layout.padding,
+                  left: layout.padding,
+                  right: layout.padding,
+                ),
+                child: ListView.builder(
+                    itemCount: snapshot.data?.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return JobTile(
+                        jobID: snapshot.data.docs[index]['job_id'],
+                        jobTitle: snapshot.data.docs[index]['title'],
+                        jobDesc: snapshot.data.docs[index]['desc'],
+                        uploadedBy: snapshot.data.docs[index]['id'],
+                        contactName: snapshot.data.docs[index]['name'],
+                        contactImage: snapshot.data.docs[index]['user_image'],
+                        contactEmail: snapshot.data.docs[index]['email'],
+                        jobLocation: snapshot.data.docs[index]['address'],
+                        recruiting: snapshot.data.docs[index]['recruiting'],
+                      );
+                    }),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(layout.padding * 6),
+                child: Center(
+                  child: Image.asset('assets/images/empty.png'),
+                ),
+              );
+            }
+          } else {
+            return Center(
+              child: Text(
+                'FATAL ERROR',
+                style: txt.error,
               ),
-            ),
-          );
+            );
+          }
         },
       ),
+    );
+  }
+
+  //job filtering
+
+  _showJobCategoriesDialog() {
+    Size size = MediaQuery.of(context).size;
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.black54,
+          title: Padding(
+            padding: const EdgeInsets.only(
+              top: layout.padding,
+              bottom: layout.padding,
+            ),
+            child: Text(
+              'Job Categories',
+              textAlign: TextAlign.center,
+              style: txt.titleLight.copyWith(color: clr.passiveLight),
+            ),
+          ),
+          content: SizedBox(
+            width: size.width * 0.9,
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: jobCategories.length,
+              itemBuilder: ((context, index) {
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      jobCategoryFilter = jobCategories[index];
+                    });
+                    Navigator.canPop(context) ? Navigator.pop(context) : null;
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      bottom: index != jobCategories.length - 1
+                          ? layout.padding
+                          : 0,
+                    ),
+                    child: Row(children: [
+                      Icon(
+                        Icons.business,
+                        color: clr.passiveLight,
+                        size: 25.0,
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: layout.padding * 1.25,
+                          ),
+                          child: Text(
+                            jobCategories[index],
+                            style: txt.body2Light
+                                .copyWith(color: clr.passiveLight),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                );
+              }),
+            ),
+          ),
+          actions: [
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    jobCategoryFilter = null;
+                  });
+                  Navigator.canPop(context) ? Navigator.pop(context) : null;
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    right: layout.padding,
+                    bottom: layout.padding * 2,
+                  ),
+                  child: Row(children: [
+                    Icon(
+                      Icons.clear_all,
+                      color: clr.passiveLight,
+                      size: layout.iconSmall,
+                    ),
+                    const Text(
+                      ' Clear Filter',
+                      style: txt.body1Light,
+                    ),
+                  ]),
+                ),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                InkWell(
+                  onTap: () {
+                    // setState(() {
+                    //   jobCategoryFilter = null;
+                    // });
+                    Navigator.canPop(context) ? Navigator.pop(context) : null;
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: layout.padding,
+                      bottom: layout.padding * 2,
+                    ),
+                    child: Row(children: [
+                      Icon(
+                        Icons.close,
+                        color: clr.passiveLight,
+                        size: layout.iconSmall,
+                      ),
+                      const Text(
+                        ' Close',
+                        style: txt.button,
+                      ),
+                    ]),
+                  ),
+                ),
+              ]),
+            ]),
+          ],
+        );
+      },
     );
   }
 }
