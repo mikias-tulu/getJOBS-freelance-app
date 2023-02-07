dc208085fc0f77a1a4ceee6f7fc2d12a30342359
Miki T <miki.tulu@gmail.com>
2023-02-07 23:32:54 +0300

    initial commit to profile_company.dart

---
+++ lib/screens/profile/profile_company.dart
@@ -0,0 +1,322 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:freelance_app/config/user_state.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ProfileScreen extends StatefulWidget {
+  final String userID;
+
+  const ProfileScreen({required this.userID});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  bool _isLoading = false;
+  String phoneNumber = "";
+  String email = "";
+  String? name;
+  String imageUrl = "";
+  String joinedAt = " ";
+  bool _isSameUser = false;
+
+  void getUserData() async {
+    try {
+      _isLoading = true;
+      final DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.userID)
+          .get();
+      if (userDoc == null) {
+        return;
+      } else {
+        setState(() {
+          email = userDoc.get('email');
+          name = userDoc.get('name');
+          phoneNumber = userDoc.get('phoneNumber');
+          imageUrl = userDoc.get('user_image');
+          Timestamp joinedAtTimeStamp = userDoc.get('created');
+          var joinedDate = joinedAtTimeStamp.toDate();
+          joinedAt = '${joinedDate.year}-${joinedDate.month}-${joinedDate.day}';
+        });
+        User? user = _auth.currentUser;
+        final _uid = user!.uid;
+        setState(() {
+          _isSameUser = _uid == widget.userID;
+        });
+      }
+    } catch (eror) {
+    } finally {
+      _isLoading = false;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 0),
+                  child: Stack(
+                    children: [
+                      Card(
+                        color: Colors.white10,
+                        margin: const EdgeInsets.all(30),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(
+                                height: 100,
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: Text(name == null ? 'Name here' : name!,
+                                    style: const TextStyle(
+                                        color: Color(0xff044404),
+                                        fontSize: 22.0)),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Divider(
+                                thickness: 1,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  'Account Information :',
+                                  style: TextStyle(
+                                      color: Color(0xff044404), fontSize: 22.0),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child:
+                                    userInfo(icon: Icons.email, content: email),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: userInfo(
+                                    icon: Icons.phone_android,
+                                    content: phoneNumber),
+                              ),
+                              SizedBox(
+                                height: 35,
+                              ),
+                              Divider(
+                                thickness: 1,
+                                color: Colors.white,
+                              ),
+                              _isSameUser
+                                  ? Container()
+                                  : Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        _contactBy(
+                                          color: Colors.green,
+                                          fct: () {
+                                            _openWhatsAppChat();
+                                          },
+                                          icon: FontAwesome.whatsapp,
+                                        ),
+                                        _contactBy(
+                                            color: Colors.red,
+                                            fct: () {
+                                              _mailTo();
+                                            },
+                                            icon: Icons.mail_outline),
+                                        _contactBy(
+                                            color: Colors.purple,
+                                            fct: () {
+                                              _callPhoneNumber();
+                                            },
+                                            icon: Icons.call_outlined),
+                                      ],
+                                    ),
+                              SizedBox(
+                                height: 25,
+                              ),
+                              Divider(
+                                thickness: 1,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                height: 25,
+                              ),
+                              !_isSameUser
+                                  ? Container()
+                                  : Center(
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 30),
+                                        child: MaterialButton(
+                                          onPressed: () {
+                                            _auth.signOut();
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) =>
+                                                    UserState(),
+                                              ),
+                                            );
+                                          },
+                                          color: Color(0xff044404),
+                                          elevation: 8,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(13)),
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 14),
+                                            child: Row(
+                                              mainAxisSize: MainAxisSize.min,
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text(
+                                                  'Logout',
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 20),
+                                                ),
+                                                SizedBox(
+                                                  width: 8,
+                                                ),
+                                                Icon(
+                                                  Icons.logout,
+                                                  color: Colors.white,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: size.width * 0.26,
+                            height: size.width * 0.26,
+                            decoration: BoxDecoration(
+                              color: Color(0xff044404),
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 8,
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor,
+                              ),
+                              image: DecorationImage(
+                                  image: NetworkImage(imageUrl == null
+                                      ? 'https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png'
+                                      : imageUrl),
+                                  fit: BoxFit.fill),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+
+  void _openWhatsAppChat() async {
+    var url = 'https://wa.me/$phoneNumber?text=HelloWorld';
+    launch(url);
+  }
+
+  void _mailTo() async {
+    final Uri params = Uri(
+      scheme: 'mailto',
+      path: email,
+      query:
+          'subject=Write subject here, please&body = hello, please write details here',
+    );
+    final url = params.toString();
+    launch(url);
+  }
+
+  void _callPhoneNumber() async {
+    var url = 'tel://$phoneNumber';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Error occurred';
+    }
+  }
+
+  Widget _contactBy(
+      {required Color color, required Function fct, required IconData icon}) {
+    return CircleAvatar(
+      backgroundColor: color,
+      radius: 25,
+      child: CircleAvatar(
+          radius: 23,
+          backgroundColor: Colors.white,
+          child: IconButton(
+            icon: Icon(
+              icon,
+              color: color,
+            ),
+            onPressed: () {
+              fct();
+            },
+          )),
+    );
+  }
+
+  Widget userInfo({required IconData icon, required String content}) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: Colors.white,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            content,
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+      ],
+    );
+  }
+}
