aa2b7e6b3cc8c294f132429c8f1ba409cd7e1206
Miki T <miki.tulu@gmail.com>
2023-02-18 18:04:31 +0300

    filter capacity for postd jobs

--- lib/screens/homescreen/components/posted_jobs.dart
+++ lib/screens/homescreen/components/posted_jobs.dart
@@ -40,65 +40,107 @@ class _PostedjobState extends State<Postedjob> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance
-            .collection('jobPosted')
-            .where('category', isEqualTo: jobCategoryFilter)
-            .where('recruiting', isEqualTo: true)
-            .orderBy('created', descending: false)
-            .snapshots(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.connectionState == ConnectionState.active) {
-            if (snapshot.data?.docs.isNotEmpty == true) {
-              return Padding(
-                padding: const EdgeInsets.only(
-                  top: 0,
-                  bottom: layout.padding,
-                  left: layout.padding,
-                  right: layout.padding,
-                ),
-                child: ListView.builder(
-                    itemCount: snapshot.data?.docs.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return JobTile(
-                        jobID: snapshot.data.docs[index]['job_id'],
-                        jobTitle: snapshot.data.docs[index]['title'],
-                        jobDesc: snapshot.data.docs[index]['desc'],
-                        uploadedBy: snapshot.data.docs[index]['id'],
-                        contactName: snapshot.data.docs[index]['name'],
-                        contactImage: snapshot.data.docs[index]['user_image'],
-                        contactEmail: snapshot.data.docs[index]['email'],
-                        jobLocation: snapshot.data.docs[index]['address'],
-                        recruiting: snapshot.data.docs[index]['recruiting'],
-                      );
-                    }),
-              );
-            } else {
-              return Padding(
-                padding: const EdgeInsets.all(layout.padding * 6),
-                child: Center(
-                  child: Image.asset('assets/images/empty.png'),
-                ),
-              );
-            }
-          } else {
-            return Center(
-              child: Text(
-                'FATAL ERROR',
-                style: txt.error,
+        child: Column(
+      children: [
+        Expanded(
+          flex: 0,
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 10,
               ),
-            );
-          }
-        },
-      ),
-    );
+              IconButton(
+                onPressed: () {
+                  showJobCategoriesDialog();
+                },
+                icon: const Icon(
+                  Icons.filter_list,
+                  color: clr.primary,
+                  size: layout.iconMedium,
+                ),
+              ),
+              Text(
+                "Filter Jobs based on your choice",
+                style: txt.body2Dark.copyWith(color: Colors.grey),
+                //TextStyle(fontSize: 15, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          child: jobCategoryFilter != null
+              ? Text(
+                  jobCategoryFilter.toString(),
+                  style: txt.body2Dark,
+                )
+              : const Text(
+                  "Recent Jobs",
+                  style: txt.body2Dark,
+                ),
+        ),
+        Expanded(
+          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: FirebaseFirestore.instance
+                .collection('jobPosted')
+                .where('category', isEqualTo: jobCategoryFilter)
+                .where('recruiting', isEqualTo: true)
+                .orderBy('created', descending: true)
+                .snapshots(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.data?.docs.isNotEmpty == true) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                      top: 0,
+                      bottom: layout.padding,
+                      left: layout.padding,
+                      right: layout.padding,
+                    ),
+                    child: ListView.builder(
+                        itemCount: snapshot.data?.docs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return JobTile(
+                            jobID: snapshot.data.docs[index]['job_id'],
+                            jobTitle: snapshot.data.docs[index]['title'],
+                            jobDesc: snapshot.data.docs[index]['desc'],
+                            uploadedBy: snapshot.data.docs[index]['id'],
+                            contactName: snapshot.data.docs[index]['name'],
+                            contactImage: snapshot.data.docs[index]
+                                ['user_image'],
+                            contactEmail: snapshot.data.docs[index]['email'],
+                            jobLocation: snapshot.data.docs[index]['address'],
+                            recruiting: snapshot.data.docs[index]['recruiting'],
+                          );
+                        }),
+                  );
+                } else {
+                  return Padding(
+                    padding: const EdgeInsets.all(layout.padding * 6),
+                    child: Center(
+                      child: Image.asset('assets/images/empty.png'),
+                    ),
+                  );
+                }
+              } else {
+                return Center(
+                  child: Text(
+                    'FATAL ERROR',
+                    style: txt.error,
+                  ),
+                );
+              }
+            },
+          ),
+        ),
+      ],
+    ));
   }
 
   //job filtering
 
-  _showJobCategoriesDialog() {
+  showJobCategoriesDialog() {
     Size size = MediaQuery.of(context).size;
     showDialog(
       context: context,
