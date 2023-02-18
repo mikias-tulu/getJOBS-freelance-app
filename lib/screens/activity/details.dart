4b56d314fc58e3147e660f3cad36e1cf77d99042
Miki T <miki.tulu@gmail.com>
2023-02-18 18:02:12 +0300

    created details page for jobs posted

---
+++ lib/screens/activity/details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class details extends StatelessWidget {
+  const details({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Details'),
+      ),
+      body: Center(child: Text('Details')),
+    );
+  }
+}
