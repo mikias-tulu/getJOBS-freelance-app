f941a19eacdfb0465fc767d3b2b3bf92e46ccaf7
Miki T <miki.tulu@gmail.com>
2023-02-21 20:06:17 +0300

    initial commit to activity_job_taken.dart

---
+++ lib/screens/activity/activity_jobs_taken.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'jobs-card.dart';
+
+class taken extends StatefulWidget {
+  const taken({super.key});
+
+  @override
+  State<taken> createState() => _takenState();
+}
+
+class _takenState extends State<taken> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        /*
+        Job(
+          position: 'Graphics Designer',
+          companyName: 'RDX',
+          date: DateTime(2013, 2, 1),
+          type: 'taken',
+        ),
+        Job(
+          position: 'Flutter Developer',
+          companyName: 'ABC',
+          date: DateTime(2013, 2, 1),
+          type: 'taken',
+        ),
+        Job(
+          position: 'Web Designer',
+          companyName: 'AAU',
+          date: DateTime(2013, 2, 1),
+          type: 'taken',
+        ),
+        */
+      ],
+    );
+  }
+}
