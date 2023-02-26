6e9055c7ef54998917cafce3042a877673eb5470
Miki T <miki.tulu@gmail.com>
2023-02-26 23:19:07 +0300

    Update catagories position

--- lib/screens/homescreen/components/categories.dart
+++ lib/screens/homescreen/components/categories.dart
@@ -1,9 +1,11 @@
 import "package:flutter/material.dart";
-
-import 'package:freelance_app/utils/colors.dart';
+import 'package:freelance_app/screens/homescreen/components/posted_jobs.dart';
 
 class Category extends StatefulWidget {
-  const Category({super.key});
+  //final cata = const Postedjob();
+  const Category({
+    super.key,
+  });
 
   @override
   State<Category> createState() => _CategoryState();
@@ -11,6 +13,7 @@ class Category extends StatefulWidget {
 
 class _CategoryState extends State<Category> {
   final List<String> fields = [
+    "RECENT JOBS",
     "GRAPHICS DESIGN",
     "UI/UX",
     "WEB DEVELOPMENT",
@@ -20,35 +23,29 @@ class _CategoryState extends State<Category> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: fields.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-              height: 2,
-              margin: const EdgeInsets.all(8),
-              /*
-            decoration: BoxDecoration(
-              color: Colors.white12,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30)),
-            ),*/
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 12,
-                  left: 18,
-                  right: 5,
-                ),
-                child: Text(fields[index],
-                    style: const TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.bold)),
-              ));
-        },
+    return GestureDetector(
+      onTap: () {},
+      child: SizedBox(
+        height: 60,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: fields.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+                height: 2,
+                margin: const EdgeInsets.all(8),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 12,
+                    left: 18,
+                    right: 5,
+                  ),
+                  child: Text(fields[index],
+                      style: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.bold)),
+                ));
+          },
+        ),
       ),
     );
   }
